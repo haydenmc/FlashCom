fc9066b3618e39641374faccf85f580936e45ca9
Hayden McAfee <hayden@outlook.com>
2024-07-29 19:46:46 -0700

    📂 Fix ShellExecute to properly determine working directory (#25)
    ShellExecute commands would previously always pass a null starting
    directory to the
    [ShellExecuteW](https://learn.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-shellexecutew)
    function, resulting in applications getting launched in FlashCom's
    working directory, breaking apps that load binaries or other resources
    from their directory on disk.
    
    This change introduces a new optional JSON property `"executeDirectory"`
    that allows a user to specify a starting directory.
    
    If a starting directory is not provided, the specified file path is used
    to determine the working directory. If the path does not provide a
    parent directory, then a null value is passed to `ShellExecuteW`.

--- src/FlashCom/Models/ShellExecuteTreeNode.cpp
+++ src/FlashCom/Models/ShellExecuteTreeNode.cpp
@@ -1,11 +1,38 @@
 #include <pch.h>
 #include "ShellExecuteTreeNode.h"
 
+namespace
+{
+    std::optional<std::filesystem::path> GetExecuteDirectory(std::string_view file,
+        std::string_view directory)
+    {
+        if (directory.length() > 0)
+        {
+            return directory;
+        }
+
+        // Default to the folder containing the file if one exists
+        std::filesystem::path filePath{ file };
+        if (filePath.has_parent_path())
+        {
+            auto parentPath{ filePath.parent_path() };
+            SPDLOG_INFO("::GetExecuteDirectory - No execute directory defined for '{}', "
+                "defaulting to '{}'", file, parentPath.string());
+            return parentPath;
+        }
+
+        SPDLOG_INFO("::GetExecuteDirectory - No execute directory defined for '{}', "
+            "defaulting to null.", file);
+        return std::nullopt;
+    }
+}
+
 namespace FlashCom::Models
 {
     ShellExecuteTreeNode::ShellExecuteTreeNode(uint32_t vkCode, std::string_view name,
-        std::string_view file, std::string_view parameters) : TreeNode{ vkCode, name, {} },
-        m_file{ file }, m_parameters{ parameters }
+        std::string_view file, std::string_view parameters, std::string_view directory) :
+        TreeNode{ vkCode, name, {} }, m_file{ file }, m_parameters{ parameters },
+        m_directory{ GetExecuteDirectory(file, directory) }
     { }
     void ShellExecuteTreeNode::Execute() const
     {
@@ -16,7 +43,8 @@ namespace FlashCom::Models
             L"open",
             reinterpret_cast<wchar_t*>(fileString.data()),
             reinterpret_cast<wchar_t*>(parametersString.data()),
-            nullptr,
+            m_directory.has_value() ?
+                reinterpret_cast<wchar_t*>(m_directory.value().wstring().data()) : nullptr,
             SW_SHOWNORMAL
         ) };
         if (reinterpret_cast<INT_PTR>(executeResult) <= 32)
